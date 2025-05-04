ec405a5f6d4f9372ff66b4477f3f7c37b36fdcb7
Epsyloun <rodrigo.diaz8b@gmail.com>
2025-05-04 10:56:11 -0600

    Subiendo configuracion inicial con react router dom y MUI

---
+++ src/utils/Const.jsx
@@ -0,0 +1,31 @@
+export const navItems = [
+  {
+    name: "Inicio",
+    to: "/",
+  },
+  {
+    name: "Historia",
+    to: "/history",
+  },
+  {
+    name: "Actividades",
+    to: "/activities",
+  },
+  {
+    name: "Restaurante",
+    to: "/restaurant",
+  },
+  {
+    name: "Galería",
+    to: "/gallery",
+  },
+  {
+    name: "Como llegar",
+    to: "/location",
+  },
+  {
+    name: "Contactanos",
+    to: "/contact",
+  },
+  { name: "Productos", to: "/products" },
+];
